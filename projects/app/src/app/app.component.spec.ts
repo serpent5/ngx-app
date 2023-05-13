07a76331d1b8ae6b9dec87056522204458b178cd
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2023-05-13 10:14:51 +0100

    ng g app app --routing=false --style=css

---
+++ projects/app/src/app/app.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () => {
+  let componentFixture: ComponentFixture<AppComponent>;
+  let componentInstance: AppComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
+
+    componentFixture = TestBed.createComponent(AppComponent);
+    componentInstance = componentFixture.componentInstance;
+  });
+
+  it("should create the app", () => expect(componentInstance).toBeTruthy());
+
+  it("should render title", () => {
+    componentFixture.detectChanges();
+
+    const el = componentFixture.nativeElement as HTMLElement;
+    expect(el.querySelector("h1")?.textContent).toContain("ngx-app");
+  });
+});
